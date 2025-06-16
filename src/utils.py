8efb69dd967666c4bdc9702a67ddf188770ea2e1
Bruno Maestri A Becker <bruno.maestri@edu.pucrs.br>
2025-06-16 17:09:47 -0300

    chore: Better parameter naming and docstring for concat_fastas function

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,16 @@
 import pathlib as p
 
 
-def concat_fastas(out: p.Path, *args: p.Path):
-    contents = [file.read_text() for file in args]
-    with out.open('w') as output:
+def concat_fastas(dst: p.Path, *fastas: p.Path):
+    """Concat a list of fasta files into one fasta.
+
+    Arguments
+    ---------
+    dst : Path
+        Destination name for the resulting fasta.
+    *fastas : Path
+        Fasta files that should be concated together.
+    """
+    contents = [file.read_text() for file in fastas]
+    with dst.open('w') as output:
         output.writelines('\n'.join(contents))
